ba8bff314d1f3b2053ba9c2afd346c6a5979f258
Vitor Estevam <vitorestevam02@gmail.com>
2021-06-18 14:40:18 -0300

    added image and new languages on model

--- guidesapp/models.py
+++ guidesapp/models.py
@@ -6,10 +6,13 @@ class Guide(models.Model):
     class Meta:
         db_table = 'guides'
 
+    image = models.ImageField()
     name = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     alt_description = models.CharField(max_length=300)
-    audio_description = models.FileField()
+    audio_description_pt = models.FileField()
+    audio_description_en = models.FileField()
+    audio_description_es = models.FileField()
 
     def __str__(self):
         return self.name
