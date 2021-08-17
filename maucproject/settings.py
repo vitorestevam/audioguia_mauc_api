ca47fa9134d3c96e6eac3fb089ed707c580787d8
Vitor Estevam <vitorestevam02@gmail.com>
2021-08-17 08:50:07 -0300

    add corsheaders

--- maucproject/settings.py
+++ maucproject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'guidesapp'
+    'corsheaders',
+    'guidesapp',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'maucproject.urls'
@@ -70,9 +73,19 @@ TEMPLATES = [
     },
 ]
 
+#CORSHEADER
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:3000',
+]
+
 WSGI_APPLICATION = 'maucproject.wsgi.application'
 
-#rest framework
+# rest framework
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
